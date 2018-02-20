d5abc37ebfd9195270eaa2dec80c3e822ac34319
Sylvain Ung <sylvainung@live.com>
2018-02-19 17:34:01 -0800

    Adds table and functionality to print it to the terminal

--- ticTacToe.js
+++ ticTacToe.js
@@ -12,3 +12,18 @@ const changeUser = () => {
   currentUser = 'O';
 };
 
+// creates playing table
+const table = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+// prints playing table
+const printTable = () => {
+  console.log(`| ${table[0][0]} | ${table[0][1]} | ${table[0][2]} |\n`);
+  console.log('----------');
+  console.log(`| ${table[1][0]} | ${table[1][1]} | ${table[1][2]} |\n`);
+  console.log('----------');
+  console.log(`| ${table[2][0]} | ${table[2][1]} | ${table[2][2]} |\n`);
+}
