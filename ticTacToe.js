9bf563d2f01f79c47b68d5df4a2e7053182e8f63
Sylvain Ung <sylvainung@live.com>
2018-02-19 17:35:36 -0800

    Adds functionality to print current user and to add entries into table

--- ticTacToe.js
+++ ticTacToe.js
@@ -27,3 +27,25 @@ const printTable = () => {
   console.log('----------');
   console.log(`| ${table[2][0]} | ${table[2][1]} | ${table[2][2]} |\n`);
 }
+
+// prints which user's turn it is
+const printUserTurn = () => {
+  console.log(`It's ${currentUser}'s turn!\n`);
+};
+
+// handles user adding x's and o's to the table
+const handleEntry = (entry) => {
+  const splitEntry = entry.split('');
+  // allows for case insensitive entries
+  splitEntry[0] = splitEntry[0].toUpperCase();
+
+  let heightIndex = null;
+  let widthIndex = Number(splitEntry[1]) - 1;
+  
+  splitEntry[0] === 'A' ? heightIndex = 0 :
+  splitEntry[0] === 'B' ? heightIndex = 1 :
+  splitEntry[0] === 'C' ? heightIndex = 2 : null;
+  
+  table[heightIndex][widthIndex] !== ' ' ? null :
+  table[heightIndex][widthIndex] = currentUser;
+}
